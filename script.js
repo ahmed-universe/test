7d533d20abcb178460a6a9db5fc05325203c778c
w01155988730@gmail.com <w01155988730@gmail.com>
2024-06-25 15:25:03 +0300

    fix

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = canvas.getBoundingClientRect().width;
+canvas.height = canvas.getBoundingClientRect().height;
 
 class Render{
     /**
